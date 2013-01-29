2d62999c619286bb155bfec9987900e836d333d9
Ulpian Morina <ulpianmorina@gmail.com>
2013-01-29 02:23:08 +0000

    default info/config page updates

--- mouse/index.php
+++ mouse/index.php
@@ -1,16 +1,15 @@
-<html>
-	<head>
-		<title>mouse::config</title>
-	</head>
-	
-	<body>
-		<img src='http://labs.gurron.com/trove/mouse_logo.png' />
-		
-		<br /><br />
-		
-		<h1>Welcome to mouse</h1>
-		<p>
-			Check that your configuration is set on whiskers.json
-		</p>
-	</body>
-</html>
+<?php
+// get the router
+require_once 'cheese/router.php';
+
+$route = new router;
+
+try
+{
+	// bake our route
+	$route->bake();
+}
+catch (Exception $ex)
+{
+	echo '<b>Warning</b>: '.$ex->getMessage()."\n";
+}
