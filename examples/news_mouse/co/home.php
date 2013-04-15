382f88a64b8f8c67ef0d793ca0124be0df054d47
Ulpian Morina <ulpianmorina@gmail.com>
2013-04-16 00:05:50 +0100

    updates & examples NewsVille template
    Signed-off-by: Ulpian Morina <ulpianmorina@gmail.com>

--- examples/news_mouse/co/home.php
+++ examples/news_mouse/co/home.php
@@ -1,28 +1,18 @@
 <?php
 
 # main controller
-class home_Controller extends moco
+class home_Controller
 {
+	use moco;
+
 	function set ()
 	{
 		# things to set
+		$this->miew->page = 'home';
 	}
 
 	function render ()
 	{
-		$this->miew->pgnm = 'index';
-
-		# should be on the model
-		$whisk = file_get_contents('whiskers.json');
-
-		$config = ['whiskers' => $whisk];
-
-		$this->miew->data('config', $config);
-
-		$usrinfo = ['name' => 'Ulpian', 'age' => '21'];
-
-		$this->miew->data('user', $usrinfo);
-
 		# send respFormat
 		$this->miew->render();
 	}
